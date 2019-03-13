9efd6bb46e82b50369b193a66e9c8219e3eb6619
Shekcon <quangsang9773@gmail.com>
2019-03-13 17:00:40 +0700

    - Add show, update, delete at movies, actors pages - Have trouble on model Award and awards pages

--- imdb/movies/urls.py
+++ imdb/movies/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path('movies/', views.index, name='home'),
-    path('movies/<int:id>/', views.view_movie, name='view_movie'),
+    path('movies/view/<int:id>/', views.view_movie, name='view_movie'),
+    path('movies/update/<int:id>/', views.update_movie, name='update_movie'),
+    path('movies/delete/<int:id>/', views.delete_movie, name='delete_movie'),
+    path('actors/view/<int:id>/', views.view_actor, name='view_actor'),
+    path('actors/update/<int:id>/', views.update_actor, name='update_actor'),
+    path('actors/delete/<int:id>/', views.delete_actor, name='delete_actor'),
     path('actors/', views.view_actors, name='actors'),
     path('awards/', views.view_awards, name='awards'),
 ]
--- imdb/movies/views.py
+++ imdb/movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, get_object_or_404, HttpResponseRedirect
 from .forms import MovieForm, ActorForm
 from .models import Movies, Actors
 
@@ -17,9 +17,67 @@ def index(request):
     return render(request, 'movies/index.html', contents)
 
 def view_movie(request, id):
-    
-    # return render(request, 'movies/actors.html', contents)
-    return 
+    movie = get_object_or_404(Movies, id=id)
+    contents = {
+        "movie": movie
+    }
+    return render(request, 'movies/view_movie.html', contents)
+
+
+def update_movie(request, id):
+    instance = get_object_or_404(Movies, id=id)
+    form = MovieForm(request.POST or None, request.FILES or None, instance=instance)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('movies:home'))
+    contents = {
+        "movie": Movies.objects.get(id=id),
+        "form": form
+    }
+    return render(request, 'movies/update_movie.html', contents)
+
+
+def delete_movie(request, id):
+    movie = get_object_or_404(Movies, id=id)
+    if request.method == "POST":
+        movie.delete()
+        return HttpResponseRedirect(reverse('movies:home'))
+    contents = {
+        "movie": movie
+    }
+    return render(request, 'movies/delete_movie.html', contents)
+
+
+def view_actor(request, id):
+    actor = get_object_or_404(Actors, id=id)
+    contents = {
+        "actor": actor
+    }
+    return render(request, 'movies/view_actor.html', contents)
+
+
+def update_actor(request, id):
+    actor = get_object_or_404(Actors, id=id)
+    form = ActorForm(request.POST or None, request.FILES or None, instance=actor)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('movies:actors'))
+    contents = {
+        "actor": actor,
+        "form": form
+    }
+    return render(request, 'movies/update_actor.html', contents)
+
+
+def delete_actor(request, id):
+    actor = get_object_or_404(Actors, id=id)
+    if request.method == "POST":
+        actor.delete()
+        return HttpResponseRedirect(reverse('movies:actors'))
+    contents = {
+        "actor": actor
+    }
+    return render(request, 'movies/delete_actor.html', contents)
 
 
 def view_actors(request):
@@ -29,7 +87,6 @@ def view_actors(request):
             form.save()
     form = ActorForm()
     contents = {
-        "user": request.user,
         "form": form,
         "actors_user": Actors.objects.all()
     }
